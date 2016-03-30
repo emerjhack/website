c494682eec314bd0f2376cf22a16596a46193294
Jeffrey Yang <jeffrey0309@gmail.com>
2016-03-30 01:45:14 -0400

    updated gitignore

---
+++ webserver/__startserver.bat
@@ -0,0 +1,5 @@
+@echo off
+set DJANGO_SECRET_KEY=@dm%42gu+mryya9c8bi3u*^g0zuc&!0u4fc8wgbegwctzdw_
+set DJANGO_EMAIL_HOST_PASSWORD=dJh28!e21
+set DJANGO_EMAIL_HOST_USER=emerjhack
+python manage.py runserver
